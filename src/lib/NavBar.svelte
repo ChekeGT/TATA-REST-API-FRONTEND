30c903bed9bbd39dfc4e06cbf76950a0ff1cef63
Francisco Ezequiel Baños Ramirez <chekelosos@gmail.com>
2023-12-15 11:33:37 -0600

    Edited the styles of the popupcontainer component and added logic to show it on the navbar component

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,4 +1,16 @@
+<script>
+    import PopUpContainer from "./PopUps/PopUpContainer.svelte";
+
+    let showAddPopup = false;
+    function toggleShowAddPopUpVisibility(){
+        showAddPopup = !showAddPopup
+    }
+</script>
 <nav class="w-full bg-darkGrey h-[96px] flex flex-row justify-between px-8 items-center">
     <h1 class="text-white text-xl font-semibold">Tata Rest API Client</h1>
-    <button class="text-white bg-mainPurple p-4 rounded-3xl">+Add New Task</button>
+    <button on:click={toggleShowAddPopUpVisibility} class="text-white bg-mainPurple p-4 rounded-3xl">+Add New Task</button>
+    {#if showAddPopup}
+        <PopUpContainer autoDestructionFunction={toggleShowAddPopUpVisibility}>
+        </PopUpContainer>
+    {/if}
 </nav>
--- src/lib/PopUps/PopUpContainer.svelte
+++ src/lib/PopUps/PopUpContainer.svelte
@@ -3,7 +3,9 @@
 
     export let autoDestructionFunction;
     export const autoDesctructionContext = setContext('autoDestruction', autoDestructionFunction);
+
+    
 </script>
-<div on:click={autoDestructionFunction} class=" w-[100vw] h-[100vh] bg-blackCover absolute z-50">
+<div on:click={autoDestructionFunction} class="bg-blackCover absolute w-11 h-12 left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-[100%] h-[100%] flex justify-center items-center">
     <slot />
 </div>
